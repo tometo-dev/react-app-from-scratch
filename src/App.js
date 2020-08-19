b380109c0abadcf3cc5cb7e31fe4e75c98a1640b
Sudhanshu <perfectsudh@gmail.com>
2020-08-19 11:03:14 +0530

    Init react app from scratch

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { hot } from "react-hot-loader"
+import "./App.css"
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello World!!</h1>
+      <p>React app from scratch</p>
+    </div>
+  )
+}
+
+export default hot(module)(App)
